c2a1ca6b189b2ca24bc0bef7206e9fcd0204cc75
nirajbhayera4 <nirajbhayera4@gmail.com>
2025-08-24 10:54:35 +0530

    Add Header component and integrate into Home page
    Created a new Header component with introductory text, a generate button, and sample images. Integrated the Header into the Home page to enhance the landing experience.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { assets } from '../assets/icons/assets'
+
+const Header = () => {
+    return (
+        <div className='flex flex-col justify-center items-center text-center my-20 '><div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'><p>Best text to image generator</p>
+            <img src={assets.star_icon} alt="" /></div><h1 className='text-4xl max-w-[300p] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-cemter'>Turn text to <span className='text-blue-600'>image</span>, in seconds.</h1>
+
+            <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Turn your imagination into visual art in seconds -just type, and watch the magic happen.</p>
+
+            <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>Generate Images <img className='h-6' src={assets.star_group} alt="" /></button>
+
+
+            <div className='flex flex-wrap justify-center mt-6 gap-3'>{Array(6).fill('').map((item, index) => (
+                <img className='rounded hover:scale-105 trasition-all duration-300 cursor-pointer max-sm:w-10' src={assets.sample_img_1} alt="" key={index } width={70}/>
+            ))}</div></div>
+    )
+}
+
+export default Header
